5b229a4d6cb62ed13392f2ea63738ef13cb4f0b1
Vinod <vinodr.msec@gmail.com>
2023-06-16 20:48:47 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='develop an algorithm that support histophathologists in the detecting of',
+    author='Vinod',
+    license='',
+)
